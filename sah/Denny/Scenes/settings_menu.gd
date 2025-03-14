6f2f4bdee6039d123339221128204fad2ce80417
HappyDie77 <soodenny123@gmail.com>
2025-03-14 20:05:09 +0100

    hjsadaw

---
+++ sah/Denny/Scenes/settings_menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+
+func _on_return_to_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Denny/Scenes/main_menu.tscn")
--- sah/Denny/Scripts/main_menu.gd
+++ sah/Denny/Scripts/main_menu.gd
@@ -1,11 +1,13 @@
 extends Control
 
 
-
-func _ready() -> void:
-	pass
-
-
-
 func _on_start_pressed() -> void:
-	
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Denny/Scenes/settings_menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
